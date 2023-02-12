d8b3ad945ff6580426fb302291814876aed1d52a
rachetnclanks <123425436+rachetnclanks@users.noreply.github.com>
2023-02-12 21:09:16 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,4 @@
+function navbar(){
+    var navbar = ("<a href=index.html>Home</a><a href=Callofduty.html>Call Of Duty</a> <a href=apex.html>Apex</a><a href=battlefield2042.html>Battlefield 2042</a><a href=valorant.html>Valorant</a><a href=E.sports.html>E.Sports</a>");
+    return navbar;
+}
